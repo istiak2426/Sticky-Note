b3a6b3f56785a61f72f731c56c244fb87604ac09
istiak.shanto <55758239+istiak2426@users.noreply.github.com>
2021-12-06 18:49:39 +0600

    Create main.js

---
+++ main.js
@@ -0,0 +1,50 @@
+     const addButton = document.querySelector(".add")
+
+     addButton.addEventListener('click', () => addNewNotes())
+
+     function addNewNotes(text= '')
+     {
+         const note = document.createElement('div')
+         note.classList.add("note")
+          note.innerHTML = `
+        
+            <div class="tools">
+
+                <button class="edit"><i class="fas fa-edit"></i></button>
+                <button class="delete"><i class="fas fa-trash-alt"></i></button>
+            </div>
+                <div class="main ${text ? "" : "hidden"}"></div>
+        <textarea ${text ? "hidden": ""}></textarea>
+            </div>
+            `
+
+            const editBtn = note.querySelector(".edit");
+            const deleteBtn = note.querySelector(".delete");
+            const main = note.querySelector(".main");
+            const textArea = note.querySelector("textarea")
+
+            
+
+            deleteBtn.addEventListener('click', () =>{
+                note.remove()
+            })
+
+
+            editBtn.addEventListener('click', () =>{
+                main.classList.toggle("hidden")
+                textArea.classList.toggle("hidden")
+            })
+
+
+            
+            
+
+
+            
+
+           
+            document.body.appendChild(note)
+
+
+
+     }
